173552215c67bda0b0e76fc507c971689795ca83
avi-11 <avi1109cool@gmail.com>
2022-01-30 01:58:28 +0530

    --2 sections--

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
 import "./App.css";
 import logo from "./logo.png";
 import decore from "./Decore.png";
+import image from "./Image.png";
+import dec from "./dec.png";
+import play from "./play.png";
+import img1 from "./img1.png";
+import card from "./card.png";
+import dest1 from "./dest1.png";
+import cardDecore from "./cardDecore.png";
 
 function App() {
   return (
@@ -23,6 +30,125 @@ function App() {
           </select>
         </div>
       </nav>
+      <section className="section1">
+        <div className="section1_write">
+          <p className="head1">BEST DESTINATIONS AROUND THE WORLD</p>
+          <p className="head2">
+            <img className="lineDec" src={dec} alt="d" />
+            Travel, enjoy and live a new and full life
+          </p>
+          <p className="head3">
+            Built Wicket longer admire do barton vanity itself do in it.
+            Preferred to sportsman it engrossed listening. Park gate sell they
+            west hard for the
+          </p>
+          <div className="decButtons">
+            <button className="findButton">Find out more</button>
+            <img className="playButton" src={play} alt="d" />
+            <span className="play">play demo</span>
+          </div>
+        </div>
+        <div className="decoreBig">
+          <img className="image" src={image} alt="sd" />
+        </div>
+      </section>
+      <section>
+        <div className="services">
+          <p className="category">CATEGORY</p>
+          <p className="categoryPitch">We Offer Best Services</p>
+        </div>
+        <div className="category4">
+          <div className="category2">
+            <div className="category1">
+              <img className="categoryImage" src={img1} alt="f" />
+              <p className="categoryTitle">Calculated Weather</p>
+              <p className="categoryText">
+                Built Wicket longer admire do barton vanity itself do in it.
+              </p>
+              <img className="card" src={card} alt="d" />
+            </div>
+
+            <div className="category1">
+              <img className="categoryImage" src={img1} alt="f" />
+              <p className="categoryTitle">Calculated Weather</p>
+              <p className="categoryText">
+                Built Wicket longer admire do barton vanity itself do in it.
+              </p>
+              <img className="card" src={card} alt="d" />
+            </div>
+          </div>
+          <div className="category2">
+            <div className="category1">
+              <img className="categoryImage" src={img1} alt="f" />
+              <p className="categoryTitle">Calculated Weather</p>
+              <p className="categoryText">
+                Built Wicket longer admire do barton vanity itself do in it.
+              </p>
+              <img className="card" src={card} alt="d" />
+            </div>
+            <div className="category1">
+              <img className="categoryImage" src={img1} alt="f" />
+              <p className="categoryTitle">Calculated Weather</p>
+              <p className="categoryText">
+                Built Wicket longer admire do barton vanity itself do in it.
+              </p>
+              <img className="card" src={card} alt="d" />
+            </div>
+          </div>
+        </div>
+      </section>
+      <section>
+        <div className="services">
+          <p className="category">Top Selling</p>
+          <p className="categoryPitch">Top Destinations</p>
+        </div>
+        <div className="Destination3">
+          <div className="Destination2">
+            <div className="Destination1">
+              <img className="destImg" src={dest1} alt="d" />
+              <div className="cardContent">
+                <div className="content1">
+                  <p>Rome,Italy </p>
+                  <p>$5.42K</p>
+                </div>
+                <div className="content2">
+                  <i class="fas fa-location-arrow location"></i>
+                  <p>12 days trip</p>
+                </div>
+              </div>
+            </div>
+            <div className="Destination1">
+              <img className="destImg" src={dest1} alt="d" />
+              <div className="cardContent">
+                <div className="content1">
+                  <p>Rome,Italy </p>
+                  <p>$5.42K</p>
+                </div>
+                <div className="content2">
+                  <i class="fas fa-location-arrow location"></i>
+                  <p>12 days trip</p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="Destination2">
+            <div className="Destination1">
+              <img className="destImg" src={dest1} alt="d" />
+              <div className="cardContent">
+                <div className="content1">
+                  <p>Rome,Italy </p>
+                  <p>$5.42K</p>
+                </div>
+                <div className="content2">
+                  <i class="fas fa-location-arrow location"></i>
+                  <p>12 days trip</p>
+                </div>
+              </div>
+            </div>
+            <img className="cardDecore" src={cardDecore} alt="d" />
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
